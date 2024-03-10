5b3c2601ea6e39135559210929023e3cf6ec7b65
tanaya-2023 <87536696+tanaya-2023@users.noreply.github.com>
2024-03-10 21:55:02 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull tanayab/python-flaskapp:latest2
+docker pull tanayab/python-flaskapp
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 tanayab/python-flaskapp:latest2
+docker run -d -p 5000:5000 tanayab/python-flaskapp
